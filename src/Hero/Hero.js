44ce96d1dd82afec2b5301806a94a232c92057dd
nihal@0101 <nihal@mapout.com>
2025-06-14 14:42:21 +0530

    initial template

---
+++ src/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledHeroMainWrapper } from './Hero.styled'
+
+const Hero = () => {
+  return (
+    <StyledHeroMainWrapper>
+        <h1>Testimonials made easy.</h1>
+    </StyledHeroMainWrapper>
+  )
+}
+
+export default Hero
---
+++ src/Hero/Hero.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledHeroMainWrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #ffa9a9;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
